0f2ef29d87603aad2364d9dbfed95440f9b998b3
Ramadan <ramadanhamdan98@gmail.com>
2025-11-12 15:59:58 +0700

    commit kedua tutorial livewire video actions

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire;
 
+use Faker\Factory;
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
 
 class Users extends Component
 {
+    public function createUser()
+    {
+        User::create([
+            'name' => Factory::create()->name,
+            'email' => Factory::create()->unique()->safeEmail,
+            'password' => Hash::make('password'),
+        ]);
+    }
     public function render()
     {
-        return view('livewire.users');
+        return view('livewire.users', [
+            'title' => 'User Page',
+            'users' => User::all()
+        ]);
     }
 }
